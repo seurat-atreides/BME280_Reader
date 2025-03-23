b07bb3871b77e357fed3732bb35f960ace4abf5d
Ernesto Lorenz <ernesto_lorenz@gmx.de>
2025-03-23 21:06:58 +0100

    Apply changes made after recent pull

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
  * @brief  BME280 atmospheric sensor reader/uploader to Ubidots for an ESP8266 or ESP8285
  * @version 0.1
  * @date 2022-04-15
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 /**
@@ -16,19 +16,13 @@
  * to a ubidots device that displays them in gauges.
  */
 
-#include <Arduino.h> 
+#include <Arduino.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
 #include <Ubidots.h>
-#include <credentials.h>
-
-#include <Arduino.h> 
-#include <Wire.h>
-#include <Adafruit_BME280.h>
-#include <ESP8266WiFi.h>
-#include <Ubidots.h>
-#include <credentials.h>
+#include "credentials.h"
 
 // User GPIO12 to power on the BME280
 #define BME_PWR 12
@@ -40,20 +34,19 @@
   #define BLYNK_PRINT Serial
 #else
   #define DEBUG_PRINT(x)
-#endif    
+#endif
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define MEASURE_INTERVAL (450e6) // measurement interval in usec. This value isn't precise! (~5 min)
+#define MEASURE_INTERVAL (450e6) // measurement interval in usec. This value isn't precise! (~7 min)
 
-
-#include <Arduino.h> 
-#include <Wire.h>
-//#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
-#include <ESP8266WiFi.h>
-
-IPAddress _dns(208,67, 222,  222);
+// The following variables are not required because the device is only active for a very short term
+// and then goes to sleep for ~ 7 min. No sense in assigning a static IP address.
+/*
+IPAddress _dns(208,67,222,222);
+IPAddress _ip(192,168,25,33);
+IPAddress _net(255,255,255,0);
+IPAddress _gw(192,168,25,1);
+*/
 
 // Instantiate an Ubidots object
 Ubidots ubidots(UBIDOTS_TOKEN);
@@ -67,8 +60,8 @@ char pass[] = PASSWD;
 
 /**
  * @brief Setup secction of the Arduino framework sketch.
- * 
- * 
+ *
+ *
  */
 void setup() {
 #if SERIAL_DEBUG
@@ -78,14 +71,14 @@ unsigned long ts1 = micros();
   Adafruit_BME280 bme; // Instantiates a BME280 object
   Ubidots ubidots(UBIDOTS_TOKEN); // Instantiates an Ubidots object
 
-#if SERIAL_DEBUG  
+#if SERIAL_DEBUG
   Serial.begin(74880);
   ubidots.setDebug(true);
 #endif
 
   DEBUG_PRINT("Serial comms are up");
 
-  WiFi.config(_ip, _gw, _net, _dns);
+  //WiFi.config(_ip, _gw, _net, _dns);
   WiFi.begin(ssid, pass);
   DEBUG_PRINT("WiFi is configured");
 
@@ -106,7 +99,7 @@ unsigned long ts1 = micros();
     }
     else
       BMEstarted = true;
-  } while (!BMEstarted);  
+  } while (!BMEstarted);
 
   // Set up the BME280 in forced mode to use less power
   bme.setSampling(Adafruit_BME280::MODE_FORCED,
@@ -114,7 +107,7 @@ unsigned long ts1 = micros();
                   Adafruit_BME280::SAMPLING_X1, // pressure
                   Adafruit_BME280::SAMPLING_X1, // humidity
                   Adafruit_BME280::FILTER_OFF   );
-  DEBUG_PRINT("BEM is started");            
+  DEBUG_PRINT("BEM is started");
 
   // Send the weather data to Blynk
   float h = bme.readHumidity();            // Read the humidity
@@ -126,15 +119,15 @@ unsigned long ts1 = micros();
   DEBUG_PRINT(t);
 
   float p = bme.readPressure() / 100.0F;   // Read the pressure and convert to millibar
-  DEBUG_PRINT("P: ");  
+  DEBUG_PRINT("P: ");
   DEBUG_PRINT(p);
 
   float v = ((float)ESP.getVcc())/1024; // system_get_vdd33() unit is 1/1024 V; 1.131 is the correction coefficient
-  DEBUG_PRINT("V: ");  
-  DEBUG_PRINT(v); 
+  DEBUG_PRINT("V: ");
+  DEBUG_PRINT(v);
 
   // Send the atmospheric values to Ubidots
-  ubidots.add("h", h);  
+  ubidots.add("h", h);
   ubidots.add("t", t);
   ubidots.add("p", p);
   ubidots.add("v", v);
@@ -145,8 +138,8 @@ unsigned long ts1 = micros();
   DEBUG_PRINT("Data sent to Ubidots");
 
   // Power off the BME280 to save energy
-  digitalWrite(BME_PWR, LOW); // Power off the BME280 before going to sleep 
-#if SERIAL_DEBUG  
+  digitalWrite(BME_PWR, LOW); // Power off the BME280 before going to sleep
+#if SERIAL_DEBUG
   Serial.print("Processing time = ");
   Serial.println((unsigned long)(micros() - ts1));
 #endif
@@ -156,10 +149,10 @@ unsigned long ts1 = micros();
 }
 
 /**
- * @brief Loop secction of the Arduino framework sketch. 
+ * @brief Loop secction of the Arduino framework sketch.
  *        This secction will not be used since we are going into deep sleep.
  */
 void loop() {
-  // Do nothing here.
+  // Do nothing here. The BME280 read process is handled by the setup() routine.
 }
 
